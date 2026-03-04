5c0d5bc9a656c87c5ed1ffd32f075711772ad1d2
hieunhoasd <hieunho500@gmail.com>
2026-03-05 01:00:43 +0700

    finish JWT

---
+++ src/main/java/HotWhellShop_Spring_react/config/JwtConfiguration.java
@@ -0,0 +1,50 @@
+package HotWhellShop_Spring_react.config;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
+import com.nimbusds.jose.util.Base64;
+
+import HotWhellShop_Spring_react.util.SecurityUtil;
+
+@Configuration
+public class JwtConfiguration {
+
+    @Value("${hieunumdum.jwt.base64-secret}")
+    private String jwtkey;
+
+    @Bean
+    public JwtEncoder jwtEncoder() {
+        return new NimbusJwtEncoder(new ImmutableSecret<>(getSecretKey()));
+    }
+
+    private SecretKey getSecretKey() {
+        byte[] keyBytes = Base64.from(jwtkey).decode();
+        return new SecretKeySpec(keyBytes, 0, keyBytes.length, SecurityUtil.JWT_ALGORITHM.getName());
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = NimbusJwtDecoder.withSecretKey(getSecretKey())
+                .macAlgorithm(SecurityUtil.JWT_ALGORITHM).build();
+        return token -> {
+            try {
+                return jwtDecoder.decode(token);
+            } catch (Exception e) {
+                System.out.println(">>> JWT error: " + e.getMessage());
+                throw e;
+            }
+        };
+    }
+
+}
---
+++ src/main/java/HotWhellShop_Spring_react/domain/Login/ResLoginDTO.java
@@ -0,0 +1,10 @@
+package HotWhellShop_Spring_react.domain.Login;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ResLoginDTO {
+    private String accessToken;
+}
