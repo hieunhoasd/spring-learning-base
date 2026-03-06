87037f8094b5c243e931f327be973585ca68653b
hieunumdum <hieunho500@gmail.com>
2026-03-06 21:38:02 +0700

    Delete HotWhellShop-react-spring-main directory

--- HotWhellShop-react-spring-main/src/main/java/HotWhellShop_Spring_react/util/error/Exception/EmailAlreadyExistsException.java
+++
@@ -1,7 +0,0 @@
-package HotWhellShop_Spring_react.util.error.Exception;
-
-public class EmailAlreadyExistsException extends RuntimeException {
-    public EmailAlreadyExistsException(String meassge) {
-        super(meassge);
-    }
-}
